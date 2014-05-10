0ea6f42cad3d38a70df638c978c3b06a2655e171
Ricardo SIGNES <rjbs@cpan.org>
2014-05-10 02:30:11 +0000

    Import of RJBS/Email-MessageID-1.405 from CPAN.
    gitpan-cpan-distribution: Email-MessageID
    gitpan-cpan-version:      1.405
    gitpan-cpan-path:         RJBS/Email-MessageID-1.405.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Email/MessageID.pm
+++ lib/Email/MessageID.pm
@@ -2,42 +2,42 @@ use strict;
 use warnings;
 package Email::MessageID;
 # ABSTRACT: Generate world unique message-ids.
-$Email::MessageID::VERSION = '1.404';
+$Email::MessageID::VERSION = '1.405';
 use overload '""' => 'as_string', fallback => 1;
 
-# =head1 SYNOPSIS
-#
-#   use Email::MessageID;
-#
-#   my $mid = Email::MessageID->new->in_brackets;
-#
-#   print "Message-ID: $mid\x0A\x0D";
-#
-# =head1 DESCRIPTION
-#
-# Message-ids are optional, but highly recommended, headers that identify a
-# message uniquely. This software generates a unique message-id.
-#
-# =method new
-#
-#   my $mid = Email::MessageID->new;
-#
-#   my $new_mid = Email::MessageID->new( host => $myhost );
-#
-# This class method constructs an L<Email::Address|Email::Address> object
-# containing a unique message-id. You may specify custom C<host> and C<user>
-# parameters.
-#
-# By default, the C<host> is generated from C<Sys::Hostname::hostname>.
-#
-# By default, the C<user> is generated using C<Time::HiRes>'s C<gettimeofday>
-# and the process ID.
-#
-# Using these values we have the ability to ensure world uniqueness down to
-# a specific process running on a specific host, and the exact time down to
-# six digits of microsecond precision.
-#
-# =cut
+#pod =head1 SYNOPSIS
+#pod
+#pod   use Email::MessageID;
+#pod
+#pod   my $mid = Email::MessageID->new->in_brackets;
+#pod
+#pod   print "Message-ID: $mid\x0A\x0D";
+#pod
+#pod =head1 DESCRIPTION
+#pod
+#pod Message-ids are optional, but highly recommended, headers that identify a
+#pod message uniquely. This software generates a unique message-id.
+#pod
+#pod =method new
+#pod
+#pod   my $mid = Email::MessageID->new;
+#pod
+#pod   my $new_mid = Email::MessageID->new( host => $myhost );
+#pod
+#pod This class method constructs an L<Email::Address|Email::Address> object
+#pod containing a unique message-id. You may specify custom C<host> and C<user>
+#pod parameters.
+#pod
+#pod By default, the C<host> is generated from C<Sys::Hostname::hostname>.
+#pod
+#pod By default, the C<user> is generated using C<Time::HiRes>'s C<gettimeofday>
+#pod and the process ID.
+#pod
+#pod Using these values we have the ability to ensure world uniqueness down to
+#pod a specific process running on a specific host, and the exact time down to
+#pod six digits of microsecond precision.
+#pod
+#pod =cut
 
 sub new {
     my ($class, %args) = @_;
@@ -50,13 +50,13 @@ sub new {
     bless \$str => $class;
 }
 
-# =method create_host
-#
-#   my $domain_part = Email::MessageID->create_host;
-#
-# This method returns the domain part of the message-id.
-#
-# =cut
+#pod =method create_host
+#pod
+#pod   my $domain_part = Email::MessageID->create_host;
+#pod
+#pod This method returns the domain part of the message-id.
+#pod
+#pod =cut
 
 my $_SYS_HOSTNAME_LONG;
 sub create_host {
@@ -69,51 +69,50 @@ sub create_host {
                                : Sys::Hostname::hostname();
 }
 
-# =method create_user
-#
-#   my $local_part = Email::MessageID->create_user;
-#
-# This method returns a unique local part for the message-id.  It includes some
-# random data and some predictable data.
-#
-# =cut
+#pod =method create_user
+#pod
+#pod   my $local_part = Email::MessageID->create_user;
+#pod
+#pod This method returns a unique local part for the message-id.  It includes some
+#pod random data and some predictable data.
+#pod
+#pod =cut
 
 my @CHARS = ('A'..'F','a'..'f',0..9);
 
-my $unique_value = 0;
-sub _generate_string {
-    my $length = 3;
-    $length = rand(8) until $length > 3;
-
-    join '', (map $CHARS[rand $#CHARS], 0 .. $length), $unique_value++;
-}
+my %uniq;
 
 sub create_user {
-    my $pseudo_random = $_[0]->_generate_string;
-    my $user = join '.', time, $pseudo_random, $$;
+    my $noise = join '',
+                map {; $CHARS[rand @CHARS] } (0 .. (3 + int rand 6));
+
+    my $t = time;
+    my $u = exists $uniq{$t} ? ++$uniq{$t} : (%uniq = ($t => 0))[1];
+
+    my $user = join '.', $t . $u, $noise, $$;
     return $user;
 }
 
-# =method in_brackets
-#
-# When using Email::MessageID directly to populate the C<Message-ID> field, be
-# sure to use C<in_brackets> to get the string inside angle brackets:
-#
-#   header => [
-#     ...
-#     'Message-Id' => Email::MessageID->new->in_brackets,
-#   ],
-#
-# Don't make this common mistake:
-#
-#   header => [
-#     ...
-#     'Message-Id' => Email::MessageID->new->as_string, # WRONG!
-#   ],
-#
-# =for Pod::Coverage address as_string host user
-#
-# =cut
+#pod =method in_brackets
+#pod
+#pod When using Email::MessageID directly to populate the C<Message-ID> field, be
+#pod sure to use C<in_brackets> to get the string inside angle brackets:
+#pod
+#pod   header => [
+#pod     ...
+#pod     'Message-Id' => Email::MessageID->new->in_brackets,
+#pod   ],
+#pod
+#pod Don't make this common mistake:
+#pod
+#pod   header => [
+#pod     ...
+#pod     'Message-Id' => Email::MessageID->new->as_string, # WRONG!
+#pod   ],
+#pod
+#pod =for Pod::Coverage address as_string host user
+#pod
+#pod =cut
 
 sub user { (split /@/, ${ $_[0] }, 2)[0] }
 sub host { (split /@/, ${ $_[0] }, 2)[1] }
@@ -147,7 +146,7 @@ Email::MessageID - Generate world unique message-ids.
 
 =head1 VERSION
 
-version 1.404
+version 1.405
 
 =head1 SYNOPSIS
 
--- xt/release/changes_has_content.t
+++ xt/release/changes_has_content.t
@@ -4,7 +4,7 @@ use Test::More tests => 2;
 
 note 'Checking Changes';
 my $changes_file = 'Changes';
-my $newver = '1.404';
+my $newver = '1.405';
 my $trial_token = '-TRIAL';
 
 SKIP: {
